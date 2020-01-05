9ce05fbd8ce7148e9cd3f3e8830deefeaf3ad5ba
paulorbsfilho <p4ulinho@outlook.com>
2020-01-04 23:09:55 -0300

    Add employer-routing.module.ts

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployerComponent } from './employer/employer.component';
+import { EmployerRoutingModule } from './employer-routing.module';
 
 
 
 @NgModule({
   declarations: [EmployerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    EmployerRoutingModule
   ],
   exports: [EmployerComponent]
 })
